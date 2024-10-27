21d021ac847c4aba4015ef7b9f3b26bf3c4245af
Rain <rain@synth.kitchen>
2024-10-27 11:13:19 +0100

    improve layout of bc players

--- src/components/releases/Releases.tsx
+++ src/components/releases/Releases.tsx
@@ -5,18 +5,32 @@ export const Releases: React.FC = () => (
   <section>
     <h2>releases</h2>
     <div className="releases">
-      <iframe
-        src="https://bandcamp.com/EmbeddedPlayer/album=820312149/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/"
-        seamless
-      >
-        <a href="https://sksksk.bandcamp.com/album/zela">Zela by Sk</a>
-      </iframe>
-      <iframe
-        src="https://bandcamp.com/EmbeddedPlayer/album=906746429/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/"
-        seamless
-      >
-        <a href="https://sksksk.bandcamp.com/album/fields">Fields by Sk</a>
-      </iframe>
+      <article>
+        <header>
+          <h3>Zela</h3>
+          <aside>2019</aside>
+        </header>
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=820312149/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          seamless
+        >
+          <a href="https://sksksk.bandcamp.com/album/zela">Zela by Sk</a>
+        </iframe>
+        <p></p>
+      </article>
+      <article>
+        <header>
+          <h3>Fields</h3>
+          <aside>2018</aside>
+        </header>
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=906746429/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+          seamless
+        >
+          <a href="https://sksksk.bandcamp.com/album/fields">Fields by Sk</a>
+        </iframe>
+        <p></p>
+      </article>
     </div>
   </section>
 );
