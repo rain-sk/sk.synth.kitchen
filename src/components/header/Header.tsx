2485a83bba02d16216cc80dd2138ad953834809b
Rain <rain@synth.kitchen>
2024-10-27 10:55:05 +0100

    improve header layout on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,10 +11,10 @@ export const Header: React.FC = () => (
       <h1>
         <Link to="/">Sk</Link>
       </h1>
-      <p>Producer | DJ | Music Technologist</p>
+      <p>Producer | DJ | Music&nbsp;Technologist</p>
       <p>
-        <a href="mailto:contact@synth.kitchen">Reach Out</a> |{" "}
-        <a href="mailto:booking@synth.kitchen">Booking Inquiries</a>
+        <a href="mailto:contact@synth.kitchen">Reach&nbsp;Out</a> |{" "}
+        <a href="mailto:booking@synth.kitchen">Booking&nbsp;Inquiries</a>
       </p>
     </div>
   </nav>
