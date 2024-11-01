df9f1c2998e6e4001a5cd39c5b5e68c8a83ebe5d
Rain <rain@synth.kitchen>
2024-11-01 12:36:08 +0100

    add the Do It Again remix to Releases

--- src/components/releases/Releases.tsx
+++ src/components/releases/Releases.tsx
@@ -7,6 +7,32 @@ export const Releases: React.FC = () => (
     <div className="releases">
       <article>
         <header>
+          <h3>Do It Again - Remix</h3>
+          <aside>2024</aside>
+        </header>
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=4189743445/size=small/bgcol=ffffff/linkcol=0687f5/track=997702670/transparent=true/"
+          seamless
+        >
+          <a href="https://liadland.bandcamp.com/album/nothing-to-declare-special-limited-edition-extended-album">
+            Nothing To Declare - Special Limited Edition Extended Album by
+            Liadland
+          </a>
+        </iframe>
+        <p>
+          You burned your hand in the fire? Do it again.
+          <br />
+          <br />
+          This dancefloor remix of Liadland's{" "}
+          <a href="https://liadland.bandcamp.com/track/do-it-again-3">
+            <i>Do It Again</i>
+          </a>{" "}
+          offers a groovy take on the original. Liadland's powerful vocals glide
+          along as the rhythm pushes you further and further over the edge.
+        </p>
+      </article>
+      <article>
+        <header>
           <h3>Zela</h3>
           <aside>2019</aside>
         </header>
