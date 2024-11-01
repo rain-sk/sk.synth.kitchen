e0e068b631a29fa2e6778719f976825c5eabc7a8
Rain <rain@synth.kitchen>
2024-11-01 11:20:19 +0100

    update profile image

--- src/components/header/ProfilePicture.tsx
+++ src/components/header/ProfilePicture.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const ProfilePicture: React.FC = () => (
   <Link id="profilePicture" to="/">
     <img
-      src="https://i1.sndcdn.com/avatars-DofzOl0NFFLxPSts-uf2Naw-t500x500.jpg"
+      src="https://i1.sndcdn.com/avatars-jquDmsmOHxTK139m-qKsjtg-t500x500.jpg"
       alt="Profile Picture"
     />
   </Link>
