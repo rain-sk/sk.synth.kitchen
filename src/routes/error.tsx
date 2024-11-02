c69c69bc21843a1ef12d1df5e26f588cba533097
Rain <rain@synth.kitchen>
2024-11-02 14:13:47 +0100

    improve the error page

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import { ParallaxBackground } from "../components/parallax-background/ParallaxBackground";
+import { Header } from "../components/header/Header";
+import { Link } from "react-router-dom";
 
 export const ErrorRoute: React.FC = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </div>
+    <>
+      <ParallaxBackground />
+      <Header />
+      <main>
+        <section>
+          <h2>404</h2>
+          <article>
+            <p>
+              Something went wrong... <Link to="/">Go home</Link>.
+            </p>
+          </article>
+        </section>
+      </main>
+    </>
   );
 };
